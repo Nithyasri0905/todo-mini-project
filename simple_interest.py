6956e6732e349e08805cbac0e77b4603158c26ff
Nithya <nithya.raichur@gmail.com>
2026-01-07 22:13:50 +0530

    first version

--- simple_interest.py
+++
@@ -1,7 +0,0 @@
-P = int(input("enter the value of p: "))
-T = int(input("enter the value of t: "))
-R = int(input("enter the value of r: "))
-
-simple_interest = (P*T*R)/100
-print(simple_interest)
-
