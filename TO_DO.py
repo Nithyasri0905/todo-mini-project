cfe024ce17911079f15436269be410bfad3215a7
Nithya <nithya.raichur@gmail.com>
2026-01-10 12:09:26 +0530

    OOP-based TODO list with file handling

--- TO_DO.py
+++ TO_DO.py
@@ -1,104 +1,129 @@
-print("TASKS FOR THE DAY!!")
-add_tasks = []
-task =" "
+#class 
+
+class TODO_list:
+    def __init__(self):
+            self.add_tasks = []
+
+            try: 
+                 with open("to_do.txt", "r") as f:
+                    for line in f:
+                        line = line.strip()
+                        if not line:
+                            continue
+
+                        if "|" in line:
+                            task_name, status = line.split("|")
+                            task = {
+                                'Task' : task_name,
+                                'Status' : status
+                            }
+
+                            self.add_tasks.append(task)
+
+                        else:
+                            print(f"skipping invalid line: {line}")
+
+            except FileNotFoundError :
+                pass 
+        
+
+    def add(self):
+
+        task_input = input("enter tasks: ")
+        task = {
+            "Task": task_input,
+            "Status": "pending"
+        }
+        self.add_tasks.append(task)
+
+        try :
+            with open("to_do.txt", "a") as f:
+                f.write(f"{task_input}|pending\n")
+
+        except PermissionError:
+            print("Error: You do not have permission to write to this file")
 
 
-def main_menu():
-    while True:
-        print("1.Add a new task")
-        print("2.view all tasks")
-        print("3.mark a task as completed")
-        print("4.delete tasks")
-        print("5.exit")
 
-        choice = input("what you wanna do?: ")
-
-        if choice == "1":
-            task = input("enter tasks: ")
-            add_tasks.append({"Task" : task , "Status" : "pending" })
-            print(add_tasks)
-            
-
-        elif choice == "2":
-            if not add_tasks:
-                print("no task added yet!")
-
-            else:
-                count = 1
-                for task in add_tasks:
-                    print(f"{count}.{task['Task']} - {task['Status']}")
-                    count += 1
-
-
-        elif choice == "3":
-            if not add_tasks:
-                print("no tasks!!")
-            else:
-                count = 1
-                for task in add_tasks:
-                    print(f"{count}.{task['Task']} - {task['Status']}")
-                    count += 1
-
-            status_update = int(input("Enter the task number to mark completed: "))
-            status_update -= 1
-            add_tasks[status_update]["Status"] = "completed"
-            print(f"the task  {add_tasks[status_update]['Task']} is marked as completed")
-
-
-        elif choice == "4":
-            if not add_tasks:
-                print("No tasks")
-            else:
-                count = 1
-                for task in add_tasks:
-                    print(f"{count}.{task['Task']} - {task['Status']}")
-                    count += 1
-                
-            delete_task = int(input("enter the task number to be deleted: "))
-            delete_task -= 1
-            deleted_task = add_tasks.pop(delete_task)
-            print(f"the task {deleted_task['Task']} is deleted")
-
-
-        elif choice == "5":
-            print("Exiting...")
-            exit()
+    def view(self):
+        if not self.add_tasks:
+            print("no tasks added yet!")
 
         else:
-            print("Invalid choice. Try again")
+            count = 1
+            for task in self.add_tasks:
+                print(f"{count}.{task['Task']} - {task['Status']}")
+                count += 1
 
         
 
+    def mark(self):
+        self.view()
+
+        status_update = int(input("enter the number of the task to mark as completed: "))
+        status_update -= 1
+        self.add_tasks[status_update]['Status'] = "completed"
+        
+        try:
+            with open("to_do.txt", "w") as f:
+                for task in self.add_tasks:
+                    f.write(f"{task['Task']}|{task['Status']}\n")
+
+        except PermissionError:
+            print("Error: You do not have permission to write to this file")
+
+        print(f"the task {self.add_tasks[status_update]['Task']} is marked as completed. ")
+
+    def delete(self):
+        self.view()
+       
+        delete_task = int(input("enter the task number to be deleted: "))
+        delete_task -= 1
+        deleted_task = self.add_tasks.pop(delete_task)
+
+        try:
+            with open("to_do.txt", "w") as f:
+                for task in self.add_tasks:
+                    f.write(f"{task['Task']}|{task['Status']}\n")
+
+        except PermissionError:
+            print("Error: You do not have permission to write to this file")
 
 
- 
+        print(f"the task {deleted_task['Task']} is deleted")
 
 
-           
+t1 = TODO_list()
 
-                
-
-                    
-                    
-
-
-
-
-           
-
+while True:
 
     
 
+    print("MAIN MENU")
+    print("1.add tasks")
+    print("2.view tasks")
+    print("3.mark task as completed")
+    print("4.delete tasks")
+    print("5.exit")
 
+    choice = int(input("what you wanna do? "))
 
+    if choice == 1:
+        t1.add()
 
+    elif choice == 2:
+        t1.view()
 
+    elif choice == 3:
+        t1.mark()
 
-main_menu()
+    elif choice == 4:
+        t1.delete()
 
-
-
-
-
-
+    elif choice == 5:
+        print("Exiting..")
+        break
+    
+    else:
+        print("invalid choice. Try again!")
 
